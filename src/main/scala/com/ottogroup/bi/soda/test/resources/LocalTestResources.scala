019b0b8522750da3b3c3b3089710811b77155433
Dominik Benz <dominik.benz@inovex.de>
2015-04-14 15:24:29 +0200

    removed javaXML plan serialization

--- src/main/scala/com/ottogroup/bi/soda/test/resources/LocalTestResources.scala
+++ src/main/scala/com/ottogroup/bi/soda/test/resources/LocalTestResources.scala
@@ -54,7 +54,7 @@ class LocalTestResources extends TestResources {
     conf.put(LOCALMODEMAXINPUTFILES.toString, "20")
     conf.put(LOCALMODEMAXBYTES.toString, "1342177280L")
     //conf.put(PLAN_SERIALIZATION.toString(), "javaXML")
-    //conf.put(HIVE_LOG_INCREMENTAL_PLAN_PROGRESS_INTERVAL.toString(), "60000")
+    conf.put(HIVE_LOG_INCREMENTAL_PLAN_PROGRESS_INTERVAL.toString(), "60000")
     val props = conf.stringPropertyNames().toArray().map(p => s"<property><name>${p.toString}</name><value>${conf.getProperty(p.toString)}</value></property>").mkString("\n")
     Files.write(Paths.get(hiveSiteXmlPath), ("<configuration>\n" + props + "\n</configuration>").getBytes());
 
