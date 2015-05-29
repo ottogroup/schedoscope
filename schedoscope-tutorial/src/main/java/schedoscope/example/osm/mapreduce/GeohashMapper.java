8a01708a2218933316c98a87ba93bcb67400165b
Dominik Benz <dominik.benz@inovex.de>
2015-05-29 10:15:17 +0200

    fixed bug

--- schedoscope-tutorial/src/main/java/schedoscope/example/osm/mapreduce/GeohashMapper.java
+++ schedoscope-tutorial/src/main/java/schedoscope/example/osm/mapreduce/GeohashMapper.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import ch.hsr.geohash.GeoHash;
 
 public class GeohashMapper extends
-		Mapper<LongWritable, Text, LongWritable, Text> {
+		Mapper<LongWritable, Text, NullWritable, Text> {
 
 	private int hashPrecision = 12;
 
@@ -23,7 +24,7 @@ public class GeohashMapper extends
 		String hash = GeoHash.geoHashStringWithCharacterPrecision(
 				Double.valueOf(rec[4]), Double.valueOf(rec[5]), hashPrecision);
 		String output = StringUtils.join(ArrayUtils.add(rec, hash), "\t");
-		context.write(key, new Text(output));
+		context.write(NullWritable.get(), new Text(output));
 	}
 
 }
