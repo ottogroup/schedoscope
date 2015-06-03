e0cc68149f0e7de5a47885b881122c2a6827025c
Annika Leveringhaus <Annika.Leveringhaus@ottogroup.com>
2015-06-03 11:21:21 +0200

    Globally used parameters

--- schedoscope-tutorial/src/main/scala/schedoscope/example/osm/Globals.scala
+++ schedoscope-tutorial/src/main/scala/schedoscope/example/osm/Globals.scala
@@ -6,12 +6,16 @@ import java.util.Date
 import org.schedoscope.dsl.views.MonthlyParameterization
 
 object Globals {
-    def defaultHiveQlParameters(v:View ) = Map(
-    "env" -> v.env,
-    "workflow_time" -> new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(new Date),
-    "workflow_name" -> v.getClass().getName()
-//    ,
-//    "year" -> this.asInstanceOf[MonthlyParameterization].year.v.get,
-//    "month" -> this.asInstanceOf[MonthlyParameterization].month.v.get
-    )
+  def defaultHiveQlParameters(v: View) = {
+    val baseParameters = Map(
+      "env" -> v.env,
+      "workflow_time" -> new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX").format(new Date),
+      "workflow_name" -> v.getClass().getName())
+
+    if (v.isInstanceOf[MonthlyParameterization])
+      baseParameters ++ Map(
+        "year" -> v.asInstanceOf[MonthlyParameterization].year.v.get,
+        "month" -> v.asInstanceOf[MonthlyParameterization].month.v.get)
+    else baseParameters
+  }
 }
