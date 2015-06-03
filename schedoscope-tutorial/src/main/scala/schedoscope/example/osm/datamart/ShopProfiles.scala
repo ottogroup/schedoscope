09b23ae0289b1ef497ac8c02e5504103ec8f9a4b
Annika Leveringhaus <Annika.Leveringhaus@ottogroup.com>
2015-06-03 19:57:02 +0200

    layer datamart

---
+++ schedoscope-tutorial/src/main/scala/schedoscope/example/osm/datamart/ShopProfiles.scala
@@ -0,0 +1,45 @@
+package schedoscope.example.osm.datamart
+
+import org.schedoscope.dsl.View
+import org.schedoscope.dsl.views.Id
+import org.schedoscope.dsl.views.JobMetadata
+import schedoscope.example.osm.processed.Nodes
+import org.schedoscope.dsl.Parameter.p
+import org.schedoscope.dsl.views.DateParameterizationUtils.allMonths
+import org.schedoscope.dsl.transformations.HiveTransformation
+import org.schedoscope.dsl.transformations.HiveTransformation.insertInto
+import org.schedoscope.dsl.transformations.HiveTransformation.queryFromResource
+import schedoscope.example.osm.Globals._
+import org.schedoscope.dsl.Parquet
+import schedoscope.example.osm.datahub.Shops
+import schedoscope.example.osm.datahub.Trainstations
+import schedoscope.example.osm.datahub.Restaurants
+
+case class ShopProfiles() extends View
+    with Id
+    with JobMetadata {
+
+  val shop_name = fieldOf[String]
+  val shop_type = fieldOf[String]
+  val area = fieldOf[String]
+  val cnt_competitors = fieldOf[Int]
+  val cnt_restaurants = fieldOf[Int]
+  val cnt_trainstations = fieldOf[Int]
+
+  dependsOn { () => Shops() }
+  dependsOn { () => Restaurants() }
+  dependsOn { () => Trainstations() }
+
+  transformVia { () =>
+    HiveTransformation(
+      insertInto(
+        this,
+        queryFromResource("hiveql/datamart/insert_shop_profiles.sql"),
+        settings = Map("parquet.compression" -> "GZIP")))
+      .configureWith(defaultHiveQlParameters(this))
+  }
+
+  comment("Shop profiles showing number of nearby competitors, restaurants and trainstations for each shop")
+
+  storedAs(Parquet())
+}
---
+++ schedoscope-tutorial/src/test/scala/schedoscope/example/osm/datamart/ShopProfilesTest.scala
@@ -0,0 +1,74 @@
+package schedoscope.example.osm.datamart
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import schedoscope.example.osm.processed.Nodes
+import org.schedoscope.test.rows
+import org.schedoscope.dsl.Parameter.p
+import org.schedoscope.dsl.Field._
+import org.schedoscope.test.test
+import schedoscope.example.osm.datahub.Trainstations
+import schedoscope.example.osm.datahub.Restaurants
+import schedoscope.example.osm.datahub.Shops
+
+case class ShopProfilesTest() extends FlatSpec
+    with Matchers {
+
+  val shops = new Shops() with rows {
+    set(v(id, "122546"),
+      v(shop_name, "Netto"),
+      v(shop_type, "supermarket"),
+      v(area, "t1y87ki"))
+    set(v(id, "274850441"),
+      v(shop_name, "Schanzenbäckerei"),
+      v(shop_type, "bakery"),
+      v(area, "t1y87ki"))
+    set(v(id, "279023080"),
+      v(shop_name, "Edeka Linow"),
+      v(shop_type, "supermarket"),
+      v(area, "t1y77d8"))
+  }
+
+  val restaurants = new Restaurants() with rows {
+    set(v(id, "267622930"),
+      v(restaurant_name, "Cuore Mio"),
+      v(restaurant_type, "italian"),
+      v(area, "t1y06x1"))
+    set(v(id, "288858596"),
+      v(restaurant_name, "Jam Jam"),
+      v(restaurant_type, "japanese"),
+      v(area, "t1y87ki"))
+    set(v(id, "302281521"),
+      v(restaurant_name, "Walddörfer Croque Café"),
+      v(restaurant_type, "burger"),
+      v(area, "t1y17m9"))
+  }
+
+  val trainstations = new Trainstations() with rows {
+    set(v(id, "122317"),
+      v(station_name, "Hagenbecks Tierpark"),
+      v(area, "t1y140d"))
+    set(v(id, "122317"),
+      v(station_name, "Bönningstedt"),
+      v(area, "t1y87ki"))
+  }
+  
+  "datamart.ShopProfiles" should "load correctly from datahub.shops, datahub.restaurants, datahub.trainstations" in {
+    new ShopProfiles() with test {
+      basedOn(shops, restaurants, trainstations)
+      then()
+      numRows shouldBe 3
+      row(v(id) shouldBe "122546",
+        v(shop_name) shouldBe "Netto",
+        v(shop_type) shouldBe "supermarket",
+        v(area) shouldBe "t1y87ki",
+        v(cnt_competitors) shouldBe 1,
+        v(cnt_restaurants) shouldBe 1,
+        v(cnt_trainstations) shouldBe 1)
+//      row(v(id) shouldBe "274850441",
+//        v(shop_name) shouldBe "Schanzenbäckerei",
+//        v(shop_type) shouldBe "bakery",
+//        v(area) shouldBe "t1y87ki")
+    }
+  }
+}
