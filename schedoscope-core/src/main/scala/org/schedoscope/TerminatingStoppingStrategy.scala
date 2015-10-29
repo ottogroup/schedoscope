1d22a37946d416a971e510f975a4592cd5f537e1
Utz Westermann <gerdutz.westermann@ottogroup.com>
2015-10-29 14:58:53 +0100

    Added system termination and logging of termination reason in Supervisor Strategy of guardian

--- schedoscope-core/src/main/scala/org/schedoscope/TerminatingStoppingStrategy.scala
+++ schedoscope-core/src/main/scala/org/schedoscope/TerminatingStoppingStrategy.scala
@@ -3,11 +3,16 @@ package org.schedoscope
 import akka.actor.SupervisorStrategyConfigurator
 import akka.actor.AllForOneStrategy
 import akka.actor.SupervisorStrategy.Escalate
+import org.slf4j.LoggerFactory
 
-class TerminatingStoppingStrategy extends SupervisorStrategyConfigurator{
+class TerminatingStoppingStrategy extends SupervisorStrategyConfigurator {
   override def create = AllForOneStrategy() {
       case t: Throwable => {
-        t.printStackTrace()
+        val log = LoggerFactory.getLogger(classOf[TerminatingStoppingStrategy])
+        
+        log.error("Unknow exception got excalated up the supervisor hierarchy. Exception {}", t)
+        
+        System.exit(1)
         Escalate
       }
     }
