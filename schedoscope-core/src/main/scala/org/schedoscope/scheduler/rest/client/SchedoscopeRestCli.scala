a350b70cb562862a39fbadc8f0a710ea253f17de
Utz Westermann <gerdutz.westermann@ottogroup.com>
2015-11-03 17:43:59 +0100

    Minor formatting and typos

--- schedoscope-core/src/main/scala/org/schedoscope/scheduler/rest/client/SchedoscopeRestCli.scala
+++ schedoscope-core/src/main/scala/org/schedoscope/scheduler/rest/client/SchedoscopeRestCli.scala
@@ -4,13 +4,14 @@ import org.schedoscope.Schedoscope
 import org.schedoscope.scheduler.commandline.SchedoscopeCliCommandRunner
 
 /**
- * A command line client to schedoscope that issues the CLI commands passed as commandline parameters to the
+ * A command line client to schedoscope that issues the CLI commands passed as command line parameters to the
  * REST web service and renders the results.
  */
 object SchedoscopeRestCli extends App {
   val schedoscope = new SchedoscopeServiceRestClientImpl(Schedoscope.settings.host, Schedoscope.settings.port)
-  val ctrl = new SchedoscopeCliCommandRunner(schedoscope)
-  ctrl.run(args)
+
+  new SchedoscopeCliCommandRunner(schedoscope).run(args)
+
   schedoscope.shutdown()
   System.exit(0)
 }
