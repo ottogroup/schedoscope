ca9bb9667c53298018df64f77b425de1063b2b0b
Utz Westermann <gerdutz.westermann@ottogroup.com>
2015-11-17 17:50:00 +0100

    Made latestDay setting val.

--- schedoscope-core/src/main/scala/org/schedoscope/Settings.scala
+++ schedoscope-core/src/main/scala/org/schedoscope/Settings.scala
@@ -70,7 +70,7 @@ class SchedoscopeSettings(val config: Config) extends Extension {
   /**
    * The configured latest, i.e., current day for DateParamterization logic
    */
-  val latestDay = {
+  lazy val latestDay = {
     val conf = config.getString("schedoscope.scheduler.latestDay")
     if (conf == "now") {
       val now = Calendar.getInstance()
