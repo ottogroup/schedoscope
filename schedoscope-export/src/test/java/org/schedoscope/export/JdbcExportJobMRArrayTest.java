57b0c6ce019454fe53159c495005a47ad28afeda
chr richter <christian_richter@gmx.de>
2016-02-22 18:07:52 +0100

    add unit test for array complex type

--- schedoscope-export/src/test/java/org/schedoscope/export/JdbcExportJobMRArrayTest.java
+++ schedoscope-export/src/test/java/org/schedoscope/export/JdbcExportJobMRArrayTest.java
@@ -1,25 +1,9 @@
-/**
- * Copyright 2016 Otto (GmbH & Co KG)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.schedoscope.export;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,13 +15,13 @@ import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
 import org.apache.hadoop.mrunit.types.Pair;
 import org.apache.hive.hcatalog.data.HCatRecord;
+import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.schedoscope.export.outputformat.JdbcOutputWritable;
 
-public class JdbcExportJobMRTest extends HiveUnitBaseTest {
+public class JdbcExportJobMRArrayTest extends HiveUnitBaseTest {
 
 	MapDriver<WritableComparable<?>, HCatRecord, Text, NullWritable> mapDriver;
 	ReduceDriver<Text, NullWritable, JdbcOutputWritable, NullWritable> reduceDriver;
@@ -58,9 +42,9 @@ public class JdbcExportJobMRTest extends HiveUnitBaseTest {
 		mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
 		mapReduceDriver.setConfiguration(conf);
 
-		setUpHiveServer("src/test/resources/ogm_event_features_data.txt",
-				"src/test/resources/ogm_event_features.hql",
-				"ogm_event_features");
+		setUpHiveServer("src/test/resources/webtrends_event_data.txt",
+				"src/test/resources/webtrends_event.hql",
+				"webtrends_event");
 	}
 
 	@Test
@@ -68,7 +52,7 @@ public class JdbcExportJobMRTest extends HiveUnitBaseTest {
 
 		Iterator<HCatRecord> it = hcatRecordReader.read();
 		while (it.hasNext()) {
-		HCatRecord record = it.next();
+			HCatRecord record = it.next();
 			mapDriver.withInput(NullWritable.get(), record);
 		}
 		List<Pair<Text, NullWritable>> out = mapDriver.run();
@@ -76,11 +60,8 @@ public class JdbcExportJobMRTest extends HiveUnitBaseTest {
 
 		for (Pair<Text, NullWritable> p : out) {
 			String jsonData = p.getFirst().toString().split("\t")[1];
-			JSONObject json = new JSONObject(jsonData);
-			assertNotEquals(1, json.length());
-
-			String fixed = p.getFirst().toString().split("\t")[3];
-			assertEquals("app.eci.datahub.OgmEventFeatures", fixed);
+			JSONArray json = new JSONArray(jsonData);
+			assertNotEquals(0, json.length());
 		}
 	}
 
@@ -89,28 +70,10 @@ public class JdbcExportJobMRTest extends HiveUnitBaseTest {
 
 		Iterator<HCatRecord> it = hcatRecordReader.read();
 		while (it.hasNext()) {
-		HCatRecord record = it.next();
+			HCatRecord record = it.next();
 			mapReduceDriver.withInput(NullWritable.get(), record);
 		}
 		List<Pair<JdbcOutputWritable, NullWritable>> out = mapReduceDriver.run();
 		assertEquals(10, out.size());
-
-	}
-
-	@Test
-	public void testReducer() throws IOException{
-
-		// input data
-		Text key = new Text("1\ttest");
-		List<NullWritable> values = new ArrayList<NullWritable>();
-		values.add(NullWritable.get());
-
-		reduceDriver.withInput(key, values);
-		List<Pair<JdbcOutputWritable, NullWritable>> out = reduceDriver.run();
-		// assertEquals(10, out.size());
-
-		//JdbcOutputWritable outWritable = out.get(0).getFirst();
-		// assertNotNull(outWritable);
 	}
 }
-
---
+++ schedoscope-export/src/test/java/org/schedoscope/export/JdbcExportJobMRMapTest.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright 2016 Otto (GmbH & Co KG)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.schedoscope.export;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.apache.hadoop.mrunit.types.Pair;
+import org.apache.hive.hcatalog.data.HCatRecord;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.schedoscope.export.outputformat.JdbcOutputWritable;
+
+public class JdbcExportJobMRMapTest extends HiveUnitBaseTest {
+
+	MapDriver<WritableComparable<?>, HCatRecord, Text, NullWritable> mapDriver;
+	ReduceDriver<Text, NullWritable, JdbcOutputWritable, NullWritable> reduceDriver;
+	MapReduceDriver<WritableComparable<?>, HCatRecord, Text, NullWritable, JdbcOutputWritable, NullWritable> mapReduceDriver;
+
+	@SuppressWarnings("deprecation")
+	@Before
+	public void setUp() throws IOException {
+		super.setUp();
+		JdbcExportMapper mapper = new JdbcExportMapper();
+		mapDriver = MapDriver.newMapDriver(mapper);
+		mapDriver.setConfiguration(conf);
+
+		JdbcExportReducer reducer = new JdbcExportReducer();
+		reduceDriver = ReduceDriver.newReduceDriver(reducer);
+		reduceDriver.setConfiguration(conf);
+
+		mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+		mapReduceDriver.setConfiguration(conf);
+
+		setUpHiveServer("src/test/resources/ogm_event_features_data.txt",
+				"src/test/resources/ogm_event_features.hql",
+				"ogm_event_features");
+	}
+
+	@Test
+	public void testJdbcMapper() throws IOException, JSONException {
+
+		Iterator<HCatRecord> it = hcatRecordReader.read();
+		while (it.hasNext()) {
+			HCatRecord record = it.next();
+			mapDriver.withInput(NullWritable.get(), record);
+		}
+		List<Pair<Text, NullWritable>> out = mapDriver.run();
+		assertEquals(10, out.size());
+
+		for (Pair<Text, NullWritable> p : out) {
+			String jsonData = p.getFirst().toString().split("\t")[1];
+			JSONObject json = new JSONObject(jsonData);
+			assertNotEquals(0, json.length());
+
+			String fixed = p.getFirst().toString().split("\t")[3];
+			assertEquals("app.eci.datahub.OgmEventFeatures", fixed);
+		}
+	}
+
+	@Test
+	public void testMapReduce() throws IOException {
+
+		Iterator<HCatRecord> it = hcatRecordReader.read();
+		while (it.hasNext()) {
+			HCatRecord record = it.next();
+			mapReduceDriver.withInput(NullWritable.get(), record);
+		}
+		List<Pair<JdbcOutputWritable, NullWritable>> out = mapReduceDriver.run();
+		assertEquals(10, out.size());
+
+	}
+
+	@Test
+	public void testReducer() throws IOException{
+
+		// input data
+		Text key = new Text("1\ttest");
+		List<NullWritable> values = new ArrayList<NullWritable>();
+		values.add(NullWritable.get());
+
+		reduceDriver.withInput(key, values);
+		List<Pair<JdbcOutputWritable, NullWritable>> out = reduceDriver.run();
+		// assertEquals(10, out.size());
+
+		//JdbcOutputWritable outWritable = out.get(0).getFirst();
+		// assertNotNull(outWritable);
+	}
+}
+
