273afe1b49d17ba0141d1f90b18a4e8f5fd8919c
Kassem Tohme <kassem.tohme@ottogroup.com>
2016-05-10 06:18:43 -0700

    added metascope start script

---
+++ schedoscope-metascope/src/main/resources/start-metascope.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CP=""
+for D in lib/*.jar; do CP=${CP}:${D}; done
+CP=${CP}:metascope.jar
+
+if [ -z "$1" ]; then
+  echo "No argument supplied. Please set the path to schedoscope.conf, e.g. './start-metascope /apps/schedoscope/schedoscope.conf'"
+else
+  java -Xmx1024m -XX:MaxPermSize=512M -cp ${CP} -Dconfig.file=${1} org.schedoscope.metascope.Metascope
+fi
