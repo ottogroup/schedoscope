264e4af832bcdc2f0f9a059f47b0725e40e2ff13
chr richter <christian_richter@gmx.de>
2016-07-09 23:15:55 +0200

    add (s)ftp uploader

--- schedoscope-export/src/main/java/org/schedoscope/export/ftp/outputformat/CSVFileOutputCommitter.java
+++ schedoscope-export/src/main/java/org/schedoscope/export/ftp/outputformat/CSVFileOutputCommitter.java
@@ -1,29 +1,43 @@
 package org.schedoscope.export.ftp.outputformat;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputCommitter;
+import org.schedoscope.export.ftp.upload.SFTPUploader;
 
 public class CSVFileOutputCommitter extends FileOutputCommitter {
 
 	private Path outputPath;
 
+	private SFTPUploader uploader;
+
 	public CSVFileOutputCommitter(Path outputPath, TaskAttemptContext context) throws IOException {
+
 		super(outputPath, context);
 		this.outputPath = outputPath;
+
 	}
 
 	@Override
 	public void commitTask(TaskAttemptContext context) throws IOException {
 
 		super.commitTask(context);
-		FileSystem fs = outputPath.getFileSystem(context.getConfiguration());
 
 		String fileName = CSVOutputFormat.getOutputName(context);
 
-		context.getTaskAttemptID().getTaskID().getId();
+		try {
+			URI remote = new URI("ftp://vagrant:vagrant@192.168.56.101/home/vagrant/" + context.getTaskAttemptID().getTaskID().getId());
+			uploader = new SFTPUploader(remote, "vagrant", "vagrant", "", true, false, context.getConfiguration());
+
+			uploader.uploadFile(new Path(outputPath, fileName + ".deflate").toString(), remote.toString());
+
+		} catch (URISyntaxException e) {
+
+		}
+
 	}
 }
