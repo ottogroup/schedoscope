354b03ad17bfb8722c433fda72bcf6ad7cc07a89
chr richter <christian_richter@gmx.de>
2016-07-16 21:24:54 +0200

    use embedded ftp server for unit test

--- schedoscope-export/src/test/java/org/schedoscope/export/ftp/FtpExportCSVMRTest.java
+++ schedoscope-export/src/test/java/org/schedoscope/export/ftp/FtpExportCSVMRTest.java
@@ -18,6 +18,20 @@ package org.schedoscope.export.ftp;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.Authority;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.User;
+import org.apache.ftpserver.ftplet.UserManager;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.ClearTextPasswordEncryptor;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
+import org.apache.ftpserver.usermanager.UserFactory;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
@@ -29,6 +43,7 @@ import org.apache.hive.hcatalog.mapreduce.HCatInputFormat;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.schedoscope.export.HiveUnitBaseTest;
@@ -40,14 +55,25 @@ public class FtpExportCSVMRTest extends HiveUnitBaseTest {
 
 	private static final String OUTPUT_DIR = "/tmp/putput";
 
+	FtpServer server;
+
 	@Override
 	@Before
 	public void setUp() throws Exception {
+
 		super.setUp();
+		startEmbeddedFtpServer();
+	}
+
+	@Override
+	@After
+	public void tearDown() {
+		server.stop();
 	}
 
 	@Test
 	public void testFtpCSVExport() throws Exception {
+
 		setUpHiveServer("src/test/resources/test_map_data.txt", "src/test/resources/test_map.hql", "test_map");
 
 		BasicConfigurator.configure();
@@ -60,7 +86,8 @@ public class FtpExportCSVMRTest extends HiveUnitBaseTest {
 		Path outfile = new Path(OUTPUT_DIR);
 
 		CSVOutputFormat.setOutputPath(job, outfile);
-		CSVOutputFormat.setOutput(job, true, FileCompressionCodec.none, "ftp://192.168.56.101:21/", "vagrant", "vagrant", null, "testing");
+		CSVOutputFormat.setOutput(job, true, FileCompressionCodec.none, "ftp://localhost:2221/", "user1", "pass1", null,
+				"testing");
 
 		job.setMapperClass(FtpExportCSVMapper.class);
 		job.setReducerClass(Reducer.class);
@@ -96,7 +123,8 @@ public class FtpExportCSVMRTest extends HiveUnitBaseTest {
 		Path outfile = new Path(OUTPUT_DIR);
 
 		CSVOutputFormat.setOutputPath(job, outfile);
-		CSVOutputFormat.setOutput(job, true, FileCompressionCodec.gzip, "sftp://192.168.56.101:22/", "vagrant", "vagrant", null, "testing");
+		CSVOutputFormat.setOutput(job, true, FileCompressionCodec.gzip, "sftp://192.168.56.101:22/", "vagrant",
+				"vagrant", null, "testing");
 
 		job.setMapperClass(FtpExportCSVMapper.class);
 		job.setReducerClass(Reducer.class);
@@ -132,7 +160,9 @@ public class FtpExportCSVMRTest extends HiveUnitBaseTest {
 		Path outfile = new Path(OUTPUT_DIR);
 
 		CSVOutputFormat.setOutputPath(job, outfile);
-		CSVOutputFormat.setOutput(job, true, FileCompressionCodec.bzip2, "sftp://192.168.56.101:22/", "vagrant", null, "/Users/mac/work/otto/schedoscope/schedoscope-export/src/test/resources/keys/id_rsa_not_encrypted", "testing");
+		CSVOutputFormat.setOutput(job, true, FileCompressionCodec.bzip2, "sftp://192.168.56.101:22/", "vagrant", null,
+				"/Users/mac/work/otto/schedoscope/schedoscope-export/src/test/resources/keys/id_rsa_not_encrypted",
+				"testing");
 
 		job.setMapperClass(FtpExportCSVMapper.class);
 		job.setReducerClass(Reducer.class);
@@ -168,7 +198,9 @@ public class FtpExportCSVMRTest extends HiveUnitBaseTest {
 		Path outfile = new Path(OUTPUT_DIR);
 
 		CSVOutputFormat.setOutputPath(job, outfile);
-		CSVOutputFormat.setOutput(job, true, FileCompressionCodec.gzip, "sftp://192.168.56.101:22/", "vagrant", "12345", "/Users/mac/work/otto/schedoscope/schedoscope-export/src/test/resources/keys/id_rsa_encrypted", "testing");
+		CSVOutputFormat.setOutput(job, true, FileCompressionCodec.gzip, "sftp://192.168.56.101:22/", "vagrant", "12345",
+				"/Users/mac/work/otto/schedoscope/schedoscope-export/src/test/resources/keys/id_rsa_encrypted",
+				"testing");
 
 		job.setMapperClass(FtpExportCSVMapper.class);
 		job.setReducerClass(Reducer.class);
@@ -188,5 +220,37 @@ public class FtpExportCSVMRTest extends HiveUnitBaseTest {
 		while (stat.hasNext()) {
 			System.out.println(stat.next());
 		}
+	}
+
+
+	private void startEmbeddedFtpServer() throws FtpException {
+
+		PropertiesUserManagerFactory propertyFactory = new PropertiesUserManagerFactory();
+		propertyFactory.setPasswordEncryptor(new ClearTextPasswordEncryptor());
+
+		UserFactory userFactory = new UserFactory();
+		userFactory.setName("user1");
+		userFactory.setPassword("pass1");
+		userFactory.setHomeDirectory("/tmp");
+
+		List<Authority> auths = new ArrayList<Authority>();
+		Authority auth = new WritePermission();
+		auths.add(auth);
+		userFactory.setAuthorities(auths);
+
+		User user = userFactory.createUser();
+		UserManager userManager = propertyFactory.createUserManager();
+		userManager.save(user);
+
+
+		ListenerFactory listenerFactory = new ListenerFactory();
+		listenerFactory.setPort(2221);
+
+		FtpServerFactory serverFactory = new FtpServerFactory();
+		serverFactory.setUserManager(userManager);
+		serverFactory.addListener("default", listenerFactory.createListener());
+
+		server = serverFactory.createServer();
+		server.start();
 	}
 }
