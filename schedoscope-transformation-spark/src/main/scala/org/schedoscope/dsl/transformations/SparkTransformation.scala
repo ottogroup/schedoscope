8e02434a734431d326003675ef1d4d95a5767826
Utz Westermann <gerdutz.westermann@ottogroup.com>
2016-10-17 13:17:04 +0200

    First setup of Spark transformation classes

---
+++ schedoscope-transformation-spark/src/main/scala/org/schedoscope/dsl/transformations/SparkTransformation.scala
@@ -0,0 +1,81 @@
+package org.schedoscope.dsl.transformations
+
+import org.schedoscope.scheduler.service.ViewTransformationStatus
+
+/**
+  * This captures view transformation logic implemented as Spark jobs. It is expected to be available in a submittable
+  * JAR or PY file.
+  *
+  * Spark transformations use their configuration to set Spark configuration values, Spark arguments, as well as environment
+  * variables for the Spark job. This works as follows:
+  *
+  * (a) any configuration key starting with "spark." is considered a Spark configuration value.
+  * (b) any configuration key starting with "--" is considered a Spark argument. The configuration value is passed as the value of the argument
+  * (c) any other configuration key is considered an environment variable to be passed to the Spark job.
+  *
+  * @param mainJarOrPy Path to the JAR or Python file containing the Spark job
+  * @param mainClass In case of a JAR, the main class within that JAR
+  * @param applicationArgs Command line arguments to pass. Defaults to an empty list
+  * @param appName An optional logical name for the Spark job
+  * @param master Spark master setting. Defaults to "yarn-cluster".
+  * @param deployMode Spark deployment mode setting. Defaults to "cluster"
+  * @param additionalJars A list of optional JAR files to deploy with the job. Defaults to an empty list.
+  * @param additionalPys A list of optional Python files to deploy with the job. Defaults to an empty list.
+  * @param additionalFiles A list of optional files to deploy with the job. Defaults to an empty list.
+  * @param propertiesFile Path to an optional properties file.
+  */
+case class SparkTransformation(
+                                mainJarOrPy: String, mainClass: String = null,
+                                applicationArgs: List[String] = List(),
+                                appName: String = null, master: String = "yarn-cluster", deployMode: String = "cluster",
+                                additionalJars: List[String] = List(),
+                                additionalPys: List[String] = List(),
+                                additionalFiles: List[String] = List(),
+                                propertiesFile: String = null
+                              ) extends Transformation {
+  def name = "spark"
+
+  override def stringsToChecksum = if (mainClass == null) List() else List(mainClass)
+
+  override def fileResourcesToChecksum = List(mainJarOrPy)
+
+  override def viewTransformationStatus = ViewTransformationStatus(
+    name,
+    Some(Map(
+      "mainJarOrPy" -> mainJarOrPy,
+      "mainClass" -> {
+        if (mainClass == null) "" else mainClass
+      },
+      "applicationArgs" -> applicationArgs.mkString(", "),
+      "master" -> master,
+      "deployMode" -> deployMode,
+      "appName" -> {
+        if (appName == null) "" else appName
+      },
+      "additionalJars" -> additionalJars.mkString(", "),
+      "additionalPys" -> additionalPys.mkString(", "),
+      "additionalFiles" -> additionalFiles.mkString(", "),
+      "propertiesFile" -> {
+        if (propertiesFile == null) "" else propertiesFile
+      }
+    )))
+}
+
+object SparkTransformation {
+  /**
+    * Helper to return the path of a Spark job object on the Schedoscope classpath as a means to fill in the mainJarOrPy
+    * parameter of a Spark transformation
+    * @param o the Spark job object
+    * @return the path to the JAR file with the object
+    */
+  def jarOf(o: AnyRef) = o.getClass.getProtectionDomain().getCodeSource().getLocation().getFile
+
+  /**
+    * Helper to return the class name of a Spark job object on the Schedoscope classpath as a means to fill in the mainJarOrPy
+    * parameter of a Spark transformation
+    * @param o the Spark job object
+    * @return the class name
+    */
+  def nameOf(o: AnyRef) = o.getClass.getName.replaceAll("\\$$", "")
+
+}
