0c26b93d04e6eb7bb41c195205af2684d0845290
Jan Hicken <jan.hicken@ottogroup.com>
2017-02-21 17:01:14 +0100

    Add lineage validation to the test framework

--- schedoscope-core/src/main/scala/org/schedoscope/test/TestUtils.scala
+++ schedoscope-core/src/main/scala/org/schedoscope/test/TestUtils.scala
@@ -17,6 +17,8 @@ package org.schedoscope.test
 
 import org.schedoscope.dsl.FieldLike
 
+import scala.util.{Failure, Success}
+
 object TestUtils {
 
   /**
@@ -29,13 +31,22 @@ object TestUtils {
   def loadView(view: LoadableView,
                sortedBy: FieldLike[_] = null,
                disableDependencyCheck: Boolean = false,
-               disableTransformationValidation: Boolean = false) = {
+               disableTransformationValidation: Boolean = false,
+               disableLineageValidation: Boolean = false) = {
 
 
     //dependencyCheck
-    if (!disableDependencyCheck && !disableDependencyCheck) {
+    if (!disableDependencyCheck) {
       if (!view.checkDependencies()) {
         throw new IllegalArgumentException("The input views to the test given by basedOn() do not cover all types of dependencies of the view under test.")
+      }
+    }
+
+    // lineage validation
+    if (!disableLineageValidation && view.explicitLineage.isEmpty && view.dependencies.nonEmpty) {
+      view.tryLineage match {
+        case Success(_) =>
+        case Failure(ex) => throw ex
       }
     }
 
