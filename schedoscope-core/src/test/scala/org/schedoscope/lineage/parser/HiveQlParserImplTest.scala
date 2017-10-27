00f40176f00c45430f7f61967f068fdad6564463
Jan Hicken <jan.hicken@posteo.de>
2017-10-27 19:27:03 +0200

    Handle <=> like = or == in the SQL parser

---
+++ schedoscope-core/src/test/scala/org/schedoscope/lineage/parser/HiveQlParserImplTest.scala
@@ -0,0 +1,18 @@
+package org.schedoscope.lineage.parser
+
+import java.io.ByteArrayInputStream
+
+import org.apache.calcite.avatica.util.Casing
+import org.scalatest.{FlatSpec, Matchers}
+
+class HiveQlParserImplTest extends FlatSpec with Matchers {
+  "The HiveQlParserImpl" should "parse the <=> operator correctly" in {
+    val sql = "SELECT * FROM a WHERE x <=> y"
+    val stream = new ByteArrayInputStream(sql.getBytes)
+    val parser = new HiveQlParserImpl(stream)
+    parser.setIdentifierMaxLength(255)
+    parser.setUnquotedCasing(Casing.UNCHANGED)
+
+    parser.parseSqlStmtEof() should not be null
+  }
+}
